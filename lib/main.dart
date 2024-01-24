2ad563fb637d1e99a7206f259d8ef37108c36918
TranDangKhoaID <trankhoa26102002@gmail.com>
2024-01-24 09:22:07 +0700

    add firebase category

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:jokes_story/app.dart';
+import 'package:jokes_story/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
--- lib/pages/category_screen.dart
+++ lib/pages/category_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:jokes_story/model/category_model.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
@@ -9,6 +11,47 @@ class CategoryScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Categories'),
         elevation: 0,
+      ),
+      body: Column(
+        children: [
+          category(),
+        ],
+      ),
+    );
+  }
+
+  Expanded category() {
+    return Expanded(
+      child: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('categories').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+            List<DocumentSnapshot> documents = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (context, index) {
+                var document = documents[index];
+                final category = CategoryModel(
+                  id: document['id'],
+                  name: document['name'],
+                  image: document['image'],
+                );
+                return Text(category.name.toString());
+              },
+            );
+          } else {
+            // Trường hợp không có dữ liệu
+            return const Center(child: Text('No data available'));
+          }
+        },
       ),
     );
   }
